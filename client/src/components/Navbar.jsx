c76413823e3f21dd5f20bd6ecef3eca179dfdbf2
sscarry2 <earth.narongsak@gmail.com>
2022-06-26 01:21:21 +0700

    change design navbar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import logo from "../../images/logo.png";
+import { PrimaryButton, SecodaryButton } from "./Button";
 
-const menus = ["Market", "Exchange", "Tutorials", "Wallets"]
+const menus = ["Market", "Exchange", "Tutorials", "Wallets"];
 
 const NavbarItem = ({ title, classProps }) => {
   return <li className={`mx-4 cursor-pointer ${classProps}`}>{title}</li>;
@@ -13,29 +13,33 @@ const NavbarItem = ({ title, classProps }) => {
 const Navbar = () => {
   const [toggleMenu, setToggleMenu] = useState(false);
   return (
-    <nav className="w-full flex md:justify-center justify-between items-center p-4">
-      <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+    <nav className="w-full flex  justify-between items-center p-4 border-b border-[#E3E1E3]">
+      <div className="flex flex-row items-center">
+        <div className="w-8 h-8 bg-gradient-to-r from-[#EB1484] to-[#C81CC5] rounded-[10px] flex justify-center items-center">
+          <div className="w-4 h-4 rounded-full bg-transparent border-white border-2"></div>
+        </div>
+        <div className="text-[#1B1A21] font-bold ml-1">NFTMart</div>
       </div>
-      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+      <ul className="text-black md:flex hidden list-none flex-row justify-between items-center flex-initial">
         {menus.map((item, index) => (
           <NavbarItem key={item + index} title={item} />
         ))}
-        <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-          Login
-        </li>
+        <div className="flex gap-2.5">
+          <PrimaryButton title="Create" />
+          <SecodaryButton title="Connect" />
+        </div>
       </ul>
-      <div className="flex relative">
+      <div className="flex relative md:hidden">
         {toggleMenu ? (
           <AiOutlineClose
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className="text-[#888888] md:hidden cursor-pointer"
             onClick={() => setToggleMenu(false)}
           />
         ) : (
           <HiMenuAlt4
             fontSize={28}
-            className="text-white md:hidden cursor-pointer"
+            className="text-[#888888] md:hidden cursor-pointer"
             onClick={() => setToggleMenu(true)}
           />
         )}
@@ -44,11 +48,13 @@ const Navbar = () => {
             <li className="text-xl w-full my-2">
               <AiOutlineClose onClick={() => setToggleMenu(false)} />
             </li>
-            {menus.map(
-              (item, index) => (
-                <NavbarItem key={item + index} title={item} classProps="my-2 text-lg" />
-              )
-            )}
+            {menus.map((item, index) => (
+              <NavbarItem
+                key={item + index}
+                title={item}
+                classProps="my-2 text-lg"
+              />
+            ))}
           </ul>
         )}
       </div>
