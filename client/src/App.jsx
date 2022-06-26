7a09744aa9c3387b322aaf1a82b56c9bad1fd6a1
sscarry2 <earth.narongsak@gmail.com>
2022-06-27 03:22:06 +0700

    change theme design to v2

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import { Navbar, Welcome, Footer, Services, Transactions } from "./components";
+import React from "react";
+import { SecondaryButton } from "./components/atoms/Button";
+import {
+  Banner,
+  TopSellers,
+  HotBids,
+  Transactions,
+} from "./components/molecules";
+import { Navbar, Form, Footer } from "./components/organisms";
 
 const App = () => {
   return (
-    <div className="min-h-screen">
-      <div className="gradient-bg-welcome">
-        <Navbar />
-        <Welcome />
+    <div className="flex flex-col items-center min-h-screen bg-white dark:bg-[#24252D]">
+      <Navbar />
+      <div className="w-[90%] xl:w-auto">
+        <Banner />
+        <TopSellers />
+        <HotBids />
+        <SecondaryButton
+          title="Load More"
+          classProps="w-full md:w-[300px] my-[40px] md:my-[60px] mx-auto"
+        />
+        <Transactions />
       </div>
-      <Services />
-      <Transactions />
+      <Form />
       <Footer />
     </div>
   );
